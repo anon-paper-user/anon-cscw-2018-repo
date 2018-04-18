f21e3562f3880e6fa7e710b9b095934d01ce26bc
anon-paper-user <anon@no-reply.com>
2018-04-19 02:41:27 +0530

    add topic models

---
+++ drafttopic/about.py
@@ -0,0 +1,8 @@
+__name__ = "drafttopic"
+__version__ = "0.1.1"
+__author__ = []
+__author_email__ = []
+__description__ = "A library for automatic detection of topics of new " +\
+    "drafts on Wikipedia based on WikiProjects."
+__url__ = "https://github.com/anon-paper-user/anon-cscw-2018-repo"
+__license__ = "MIT"
